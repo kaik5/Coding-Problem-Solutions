8c42b35fed4892e6bff9dd3b58b8c0dbda6ea26d
kaik5 <34473749+kaik5@users.noreply.github.com>
2020-08-11 23:06:27 -0700

    Create Leetcode566.java

---
+++ Leetcode566.java
@@ -0,0 +1,32 @@
+class Solution 
+{
+    public int[][] matrixReshape(int[][] nums, int r, int c) 
+    {
+        //if the array is null or the dimension is not matched, return.
+        if(nums == null || r * c != nums.length * nums[0].length)
+            return nums;
+        int[][] output_arr = new int[r][c];
+        int new_rows = 0;
+        int new_cols = 0;
+        
+       for(int i = 0; i < nums.length; i ++)
+       {
+           for(int j = 0; j < nums[0].length; j++)
+           {
+               output_arr[new_rows][new_cols++] = nums[i][j];
+              
+               
+               //if new_cols = c, increase new_rows jump to next row and do the iteration again.
+               if(new_cols == c)
+               {
+                   new_cols = 0;
+                   new_rows++;
+                   
+               }
+           }
+       }
+        
+        return output_arr;
+        
+    }
+}
